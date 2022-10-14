c05b6049934e6b46c8beebf45c773f2b5f1d71b0
hashir.umar <hashir.umar@epesys.com>
2022-10-15 02:42:13 +0500

    Initial Commit with starter template

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import { StrictMode } from 'react';
+import ReactDOM from 'react-dom/client';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+    <StrictMode>
+        <div>Hello World!</div>
+    </StrictMode>
+);
