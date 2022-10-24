01ef04007b1507e1be28d5ec7aa720fec8e94f00
hashir.umar <hashir.umar@epesys.com>
2022-10-24 15:55:03 +0500

    refactoring

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react()],
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+
+    const processEnvValues = {
+        'process.env': Object.entries(env).reduce((prev, [key, val]) => {
+            return {
+                ...prev,
+                [key]: val,
+            };
+        }, {}),
+    };
+
+    return {
+        plugins: [react()],
+        root: '.',
+        build: {
+            outDir: './dist',
+        },
+        define: processEnvValues,
+    };
 });
